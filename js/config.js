6ec63ccf7f5be91118b0a3253797709921e0964c
ALkhaqani-Ahmed <83923766+ALkhaqani-Ahmed@users.noreply.github.com>
2024-10-30 11:19:42 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_JKhCkYw5s', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3mvlu42rlasavp0ffmfq48c70p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
